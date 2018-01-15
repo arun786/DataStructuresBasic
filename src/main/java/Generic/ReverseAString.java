e95f11f5e972de8c443498d9e1dd8c0b633551e0
arun789 <adwiti1975@gmail.com>
2018-01-14 20:27:44 -0700

    Reverse a String

---
+++ src/main/java/Generic/ReverseAString.java
@@ -0,0 +1,35 @@
+package Generic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReverseAString {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String str = br.readLine();
+            String reverse[] = toReverse(str);
+            for (int i = 0; i < reverse.length; i++) {
+                System.out.print(reverse[i]);
+            }
+
+        }
+    }
+
+    /**
+     *
+     * @param word
+     * @return
+     *
+     */
+    public static String[] toReverse(String word) {
+        String[] reverseWord = word.split("");
+        int size = reverseWord.length;
+        for (int i = 0, j = size - 1; i < size / 2; i++, j--) {
+            String temp = reverseWord[i];
+            reverseWord[i] = reverseWord[j];
+            reverseWord[j] = temp;
+        }
+        return reverseWord;
+    }
+}
